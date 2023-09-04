eb30f595508c785f097473fe7d1fbc5ca614afe7
MutableAI <mutableai@github.com>
2023-09-04 22:32:02 +0000

    [code modification] update to pages

--- templates/app.py
+++ templates/app.py
@@ -10,42 +10,40 @@ def home():
     except Exception as e:
         abort(500, description="Error occurred in rendering the home page: " + str(e))
 
-@app.route('/page1')
-def page1():
+@app.route('/menu')
+def menu():
     try:
-        return render_template('page1.html')
+        return render_template('menu.html')
     except Exception as e:
-        abort(500, description="Error occurred in rendering page 1: " + str(e))
+        abort(500, description="Error occurred in rendering the menu page: " + str(e))
 
-@app.route('/page2')
-def page2():
+@app.route('/clone_training')
+def clone_training():
     try:
-        return render_template('page2.html')
+        return render_template('clone_training.html')
     except Exception as e:
-        abort(500, description="Error occurred in rendering page 2: " + str(e))
+        abort(500, description="Error occurred in rendering the clone training page: " + str(e))
 
-@app.route('/page_count')
-def page_count():
-    # Enhanced input validation to check if pages are integers
-    def validate(value):
-        if not re.match(r"^[0-9]*$", value):
-            raise ValueError("Invalid input: " + value)
-    
+@app.route('/database_storage')
+def database_storage():
     try:
-        validate('page1')
-        validate('page2')
-
-        is_page1_complete = render_template('page1.html') is not None
-        is_page2_complete = render_template('page2.html') is not None
-        page_count = sum([is_page1_complete, is_page2_complete])
-        
-        return "The number of completed pages is: "+str(page_count)
-    
-    except ValueError as v:
-        abort(500, description="Invalid input: " + str(v))
-    
+        return render_template('database_storage.html')
     except Exception as e:
-        abort(500, description="Error occurred in getting page count: " + str(e))
+        abort(500, description="Error occurred in rendering the database/storage page: " + str(e))
+
+@app.route('/watch_page')
+def watch_page():
+    try:
+        return render_template('watch_page.html')
+    except Exception as e:
+        abort(500, description="Error occurred in rendering the watch page: " + str(e))
+
+@app.route('/banking_api')
+def banking_api():
+    try:
+        return render_template('banking_api.html')
+    except Exception as e:
+        abort(500, description="Error occurred in rendering the banking API page: " + str(e))
 
 if __name__ == '__main__':
     app.run(debug=False)  # change to False to prevent debug information disclosure.
