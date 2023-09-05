2c2a5bbe4a25b667f5e7f6722e421b981c959b00
MutableAI <mutableai@github.com>
2023-09-05 02:41:40 +0000

    [code modification] django integration apis!!!

--- templates/git_pull_request_18.sh
+++ templates/git_pull_request_18.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
+function execute_git_command() {
+    COMMAND=$1
+    eval "${COMMAND}"
+    COMMAND_EXIT_STATUS=$?
+
+    if [ ${COMMAND_EXIT_STATUS} -ne 0 ]; then
+        echo "Failed to execute: '${COMMAND}'"
+        exit ${COMMAND_EXIT_STATUS}
+    else
+        echo "Successfully executed: '${COMMAND}'"
+    fi
+}
+
+# The variable for the file to be resolved
+FILENAME=<filename>
+
 # Checkout to the PR's branch
-git checkout pull/18
+execute_git_command "git checkout pull/18"
 
 # Fetch the latest changes from the origin
-git fetch origin
+execute_git_command "git fetch origin"
 
 # Merge the main branch into the checked out branch
-git merge main
+execute_git_command "git merge main"
 
 # Fix the conflicts manually and then execute the following commands
 
 # Add the file with resolved conflicts
-git add <filename>
+execute_git_command "git add ${FILENAME}"
 
 # Commit the changes with a proper message
-git commit -m "Resolved conflicts in pull request #18"
+execute_git_command 'git commit -m "Resolved conflicts in pull request #18"'
 
 # Push the changes to the remote branch associated with PR #18
-git push origin pull/18
+execute_git_command "git push origin pull/18"
 
 # Please replace <filename> with actual file name
--- templates/pull_request_editor.py
+++ templates/pull_request_editor.py
@@ -7,29 +7,48 @@ API_URL = 'https://api.github.com'
 def get_open_pull_requests(repo_owner, repo_name):
     url = f'{API_URL}/repos/{repo_owner}/{repo_name}/pulls'
     headers = {'Authorization': f'token {ACCESS_TOKEN}'}
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
+    try: 
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+    except Exception as e:
+        print(f'Error occurred while getting pull requests: {str(e)}')
+        return []
+        
     return response.json()
 
 def edit_pull_request(repo_owner, repo_name, pull_id, title, body):
     url = f'{API_URL}/repos/{repo_owner}/{repo_name}/pulls/{pull_id}'
     headers = {'Authorization': f'token {ACCESS_TOKEN}'}
     payload = {'title': title, 'body': body}
-    response = requests.patch(url, headers=headers, json=payload)
-    response.raise_for_status()
+    try: 
+        response = requests.patch(url, headers=headers, json=payload)
+        response.raise_for_status()
+    except Exception as e:
+        print(f'Error occurred while editing pull request {pull_id}: {str(e)}')
+        return None
+    
     return response.json()
 
 def main():
     repo_owner = 'Smodal'
     repo_name = 'templates'
-    open_pull_requests = get_open_pull_requests(repo_owner, repo_name)
-    
-    for pull_request in open_pull_requests:
-        pull_id = pull_request['id']
-        title = 'Modified title'
-        body = 'Modified body'
-        edited_pull_request = edit_pull_request(repo_owner, repo_name, pull_id, title, body)
-        print(f'Edited pull request {pull_id}.')
+    try:
+        open_pull_requests = get_open_pull_requests(repo_owner, repo_name)
+    except Exception as e:
+        print(f'Error occurred while getting open pull requests: {str(e)}')
+        return
+
+    if open_pull_requests:    
+        for pull_request in open_pull_requests:
+            pull_id = pull_request['id']
+            title = 'Modified title'
+            body = 'Modified body'
+            try:
+                edited_pull_request = edit_pull_request(repo_owner, repo_name, pull_id, title, body)
+                if edited_pull_request:
+                    print(f'Edited pull request {pull_id}.')
+            except Exception as e:
+                print(f'Error occurred while editing pull request {pull_id}: {str(e)}')
 
 if __name__ == "__main__":
     main()
