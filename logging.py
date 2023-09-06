2b92b237295b961744c16d81953ff8166590bbe2
MutableAI <mutableai@github.com>
2023-09-06 23:45:44 +0000

    [code modification] adding admin tasks

--- logging.py
+++ logging.py
@@ -8,12 +8,12 @@ console_handler = logging.StreamHandler()
 file_handler = logging.FileHandler('app.log')
 
 # Set level of logging
-console_handler.setLevel(logging.WARNING)
-file_handler.setLevel(logging.ERROR)
+console_handler.setLevel(logging.INFO)  # lowered to INFO level
+file_handler.setLevel(logging.ERROR)  # kept at ERROR level
 
 # Create formatters and add it to handlers
 console_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(pathname)s - line %(lineno)d')  # Added pathname and line number information
 console_handler.setFormatter(console_format)
 file_handler.setFormatter(file_format)
 
