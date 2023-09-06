98133f2c78883f86c13acacf46fcce4b03172d75
MutableAI <mutableai@github.com>
2023-09-06 14:58:11 +0000

    [code modification] django build add clean up

--- social_media_bot.py
+++ social_media_bot.py
@@ -11,20 +11,39 @@ import social_media_api
 class SocialMediaBot:
 
     def __init__(self):
-        self.api_keys = {
+        self.api_keys = self.get_api_keys()
+
+        self.nlp = NLP()
+        self.ml = MachineLearning()
+
+        self.logger = self.setup_logger()
+
+    def get_api_keys(self):
+        return {
             "platform1": os.getenv("PLATFORM1_API_KEY"),
             "platform2": os.getenv("PLATFORM2_API_KEY"),
             # and so on for all social media platforms
         }
-        self.nlp = NLP()
-        self.ml = MachineLearning()
-        self.logger = logging.getLogger('social_bot')
-        self.logger.setLevel(logging.INFO)
+
+    def setup_logger(self):
+        logger = logging.getLogger('social_bot')
+        logger.setLevel(logging.INFO)
+
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
+
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         ch.setFormatter(formatter)
-        self.logger.addHandler(ch)
+
+        logger.addHandler(ch)
+
+        return logger
+
+    def get_access_token(self, user_id, platform_name):
+        user = get_object_or_404(User, pk=user_id)
+        platform = Platform.objects.get(name=platform_name)
+        
+        return AccessToken.objects.get(user=user, platform=platform)
 
     def authenticate(self, user_id):
         user = get_object_or_404(User, pk=user_id)
@@ -32,63 +51,42 @@ class SocialMediaBot:
             try:
                 platform = Platform.objects.get(name=platform_name)
                 access_token = social_media_api.authenticate(api_key, user_id)
+
                 AccessToken.objects.create(user=user, platform=platform, token=access_token)
             except Exception as e:
                 self.logger.error(f"Failed to authenticate {user_id} for {platform_name}: {e}")
 
-    def post_message(self, user_id, platform_name, message):
-        user = get_object_or_404(User, pk=user_id)
-        platform = Platform.objects.get(name=platform_name)
-        access_token_obj = AccessToken.objects.get(user=user, platform=platform)
+    def process_social_function(self, user_id, platform_name, func, *args):
         try:
-            social_media_api.post_message(access_token_obj.token, message)
+            access_token_obj = self.get_access_token(user_id, platform_name)
+            func(access_token_obj.token, *args)
         except Exception as e:
-            self.logger.error(f"Failed to post message for {user_id} on {platform_name}: {e}")
+            self.logger.error(f"Failed to perform {func.__name__} for {user_id} on {platform_name}: {e}")
+
+    # Hereon, the functions use the `process_social_function` to simplify their implementations
+    def post_message(self, user_id, platform_name, message):
+        self.process_social_function(user_id, platform_name, social_media_api.post_message, message)
 
     def reply_to_messages(self, user_id, platform_name, message):
-        user = get_object_or_404(User, pk=user_id)
-        platform = Platform.objects.get(name=platform_name)
-        access_token_obj = AccessToken.objects.get(user=user, platform=platform)
-        try:
-            social_media_api.reply_to_messages(access_token_obj.token, message)
-        except Exception as e:
-            self.logger.error(f"Failed to reply to messages for {user_id} on {platform_name}: {e}")
+        self.process_social_function(user_id, platform_name, social_media_api.reply_to_messages, message)
 
     def send_direct_message(self, user_id, platform_name, message):
-        user = get_object_or_404(User, pk=user_id)
-        platform = Platform.objects.get(name=platform_name)
-        access_token_obj = AccessToken.objects.get(user=user, platform=platform)
-        try:
-            social_media_api.send_direct_message(access_token_obj.token, message)
-        except Exception as e:
-            self.logger.error(f"Failed to send direct message for {user_id} on {platform_name}: {e}")
+        self.process_social_function(user_id, platform_name, social_media_api.send_direct_message, message)
 
     def update_status(self, user_id, platform_name, status):
-        user = get_object_or_404(User, pk=user_id)
-        platform = Platform.objects.get(name=platform_name)
-        access_token_obj = AccessToken.objects.get(user=user, platform=platform)
-        try:
-            social_media_api.update_status(access_token_obj.token, status)
-        except Exception as e:
-            self.logger.error(f"Failed to update status for {user_id} on {platform_name}: {e}")
+        self.process_social_function(user_id, platform_name, social_media_api.update_status, status)
 
     def share_content(self, user_id, platform_name, content):
-        user = get_object_or_404(User, pk=user_id)
-        platform = Platform.objects.get(name=platform_name)
-        access_token_obj = AccessToken.objects.get(user=user, platform=platform)
-        try:
-            social_media_api.share_content(access_token_obj.token, content)
-        except Exception as e:
-            self.logger.error(f"Failed to share content for {user_id} on {platform_name}: {e}")
+        self.process_social_function(user_id, platform_name, social_media_api.share_content, content)
 
     def monitor_new_messages(self, user_id, platform_name):
-        user = get_object_or_404(User, pk=user_id)
-        platform = Platform.objects.get(name=platform_name)
-        access_token_obj = AccessToken.objects.get(user=user, platform=platform)
         try:
+            access_token_obj = self.get_access_token(user_id, platform_name)
+
             messages = social_media_api.monitor_new_messages(access_token_obj.token)
             for message in messages:
                 response = self.nlp.analyze_message(message)
+
                 self.post_message(user_id, platform_name, response)
         except Exception as e:
             self.logger.error(f"Failed to monitor new messages for {user_id} on {platform_name}: {e}")
@@ -96,8 +94,10 @@ class SocialMediaBot:
     def revoke_access(self, user_id, platform_name):
         user = get_object_or_404(User, pk=user_id)
         platform = Platform.objects.get(name=platform_name)
+        
         try:
             social_media_api.revoke_access(user_id, platform_name)
+
             AccessToken.objects.filter(user=user, platform=platform).delete()
         except Exception as e:
             self.logger.error(f"Failed to revoke access for {user_id} on {platform_name}: {e}")
