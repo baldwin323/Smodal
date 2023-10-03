c6d4a64d9fdc927c6b194573752656672476da9a
MutableAI <mutableai@github.com>
2023-10-03 23:16:26 +0000

    [code modification] added directions,requirements

--- manage.py
+++ manage.py
@@ -1,13 +1,19 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+#!/usr/bin/env python3
+"""
+Django's command-line utility for administrative tasks.
+This script is the entry point for running the Django application and handling essential tasks such as migrations.
+"""
 import os
 import sys
 from django.core.management import execute_from_command_line
 
 from Smodal.logging import logger  # Import the centralized logger
 
+
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks.
+    This function configures the settings module and executes the command line arguments received.
+    """
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Smodal.settings')
     handle_migrations()
     execute_from_command_line(sys.argv)
@@ -19,7 +25,7 @@ def handle_migrations():
     It employs execute_from_command_line function from Django's management package to run the commands.
     In an event of an exception occurring while executing the commands, it captures it, logs a detailed message using the centralized logger, and continues to execute other commands.
 
-    Might raise: 
+    Exceptions:
     - SystemExit: If the command raises it, which might be for a number of reasons such as unapplied migrations.
     """
     try:
