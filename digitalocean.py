7e64604960a783255afef977f5d2971c47aec6e7
MutableAI <mutableai@github.com>
2023-10-14 06:49:03 +0000

    [code modification] do

--- digitalocean.py
+++ digitalocean.py
@@ -3,7 +3,7 @@ import json
 
 def get_droplets(api_key):
     headers = {
-        'Authorization': f'Bearer {api_key}',
+        'Authorization': f'Bearer {dop_v1_19b5e565d434cd27716aebb89e2f4f2d2ae90d9ef5b9f48616cfff819d8ec950}',
         'Content-Type': 'application/json',
     }
 
@@ -15,27 +15,3 @@ def get_droplets(api_key):
     else:
         print(f"Error: {response.status_code}")
         return None
-
-def create_droplet(api_key, name, region, size, image):
-    data = {
-        "name": name,
-        "region": region,
-        "size": size,
-        "image": image
-    }
-
-    headers = {
-        'Authorization': f'Bearer {api_key}',
-        'Content-Type': 'application/json',
-    }
-
-    response = requests.post('https://api.digitalocean.com/v2/droplets', headers=headers, json=data)
-
-    if response.status_code == 202:
-        return json.loads(response.text)
-    else:
-        print(f"Error: {response.status_code}")
-        return None
-
-# replace 'API_KEY', 'my-droplet-name', 'nyc1', 's-1vcpu-1gb', 'ubuntu-16-04-x64' with actual parameters
-new_droplet = create_droplet('API_KEY', 'my-droplet-name', 'nyc1', 's-1vcpu-1gb', 'ubuntu-16-04-x64')
