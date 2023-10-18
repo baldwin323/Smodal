a8d3f01600d3ba0ac10225a7b785b945c22b83f2
MutableAI <mutableai@github.com>
2023-10-18 13:51:17 +0000

    [code modification] papertrail

--- digitalocean.py
+++ digitalocean.py
@@ -3,7 +3,7 @@ import json
 
 def get_droplets(api_key):
     headers = {
-        'Authorization': f'Bearer {api_key}',
+        'Authorization': f'Bearer {dop_v1_19b5e565d434cd27716aebb89e2f4f2d2ae90d9ef5b9f48616cfff819d8ec950}',
         'Content-Type': 'application/json',
     }
 
