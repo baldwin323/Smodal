d03c444ce766371955d88d2fc96b8d3d5a9ad45b
MutableAI <mutableai@github.com>
2023-10-22 08:40:58 +0000

    [code modification] k

--- frontend.js
+++ frontend.js
@@ -1,14 +1,36 @@
-
-// Creating new file frontend.js to implement the JavaScript logic to index and scroll through pages
+/**  
+ * @file This file handles all user interaction events on the new frontend elements.
+ */
+ 
+// The index of the current page
 let currentPageIndex = 0;
+
+// The ids of the pages
 const pageIds = ['new-front-end-element1', 'new-front-end-element2', 'new-front-end-element3', 'new_feature', 'another_new_feature'];
+
+// Helper function abstracting the repetitive logic for adding event listeners and calling backend API
+function setupEventListener(id, callback) {
+    document.getElementById(id).addEventListener('click', callback);
+}
+
+// Error handling function 
+function handleErrors(response) {
+    if (!response.ok) throw Error(response.statusText);
+    return response;
+}
+
+// Callback functions calling the corresponding backend API and handling loading and errors
+const actionElements = pageIds.map(id => () => {
+    document.body.classList.add('loading');
+    fetch(`/api/${id}`).catch(handleErrors).then(() => document.body.classList.remove('loading')); 
+});
 
 document.getElementById('nav-prev').addEventListener('click', function() {
     if (currentPageIndex > 0) {
         let oldPageId = pageIds[currentPageIndex];
         currentPageIndex -= 1;
         let newPageId = pageIds[currentPageIndex];
-
+        
         document.getElementById(oldPageId).style.display = 'none';
         document.getElementById(newPageId).style.display = 'block';
     }
@@ -25,10 +47,5 @@ document.getElementById('nav-next').addEventListener('click', function() {
     }
 });
 
-// Adding event listeners to elements on the pages
-
-pageIds.forEach(id => {
-    document.getElementById(id).addEventListener('click', function(){
-        // Logic to call the corresponding backend API endpoints when an element is clicked
-    });
-});
+// Add event listeners to new elements on the watch page
+pageIds.forEach((id, index) => setupEventListener(id, actionElements[index]));
