6340801110bd8a3bb1bf71bf158f29852ac3b395
MutableAI <mutableai@github.com>
2023-10-24 05:51:26 +0000

    [code modification] ch

--- lambda_functions.py
+++ lambda_functions.py
@@ -18,6 +18,7 @@ logger.setLevel(logging.INFO)
 
 lambda_client = boto3.client('lambda')
 
+# Function to register affiliate manager with proper error handling
 def register_affiliate_manager(*args, **kwargs):
     try:
         # Updated Logic for registering affiliate manager goes here 
@@ -26,6 +27,7 @@ def register_affiliate_manager(*args, **kwargs):
         lambda_stats.increment('register_affiliate_manager.error')
         logger.error('An error occurred in register_affiliate_manager: %s', str(e))
 
+# Function to monitor affiliated models with proper error handling
 def monitor_affiliated_models(*args, **kwargs):
     try:
         # Updated Logic for monitoring affiliated models goes here
@@ -34,6 +36,7 @@ def monitor_affiliated_models(*args, **kwargs):
         lambda_stats.increment('monitor_affiliated_models.error')
         logger.error('An error occurred in monitor_affiliated_models: %s', str(e))
 
+# Function to give credit when a new model signs up with proper error handling
 def give_credit(*args, **kwargs):
     try:
         # Updated Logic for giving credit when a new model signs up goes here
@@ -43,11 +46,12 @@ def give_credit(*args, **kwargs):
         logger.error('An error occurred in give_credit: %s', str(e))
 
 operations = {
-    'register_affiliate_manager': register_affiliate_manager,
-    'monitor_affiliated_models': monitor_affiliated_models,
-    'give_credit': give_credit,
+    'register_affiliate_manager': register_affiliate_manager, # Registers affiliate manager
+    'monitor_affiliated_models': monitor_affiliated_models, # Monitors affiliated models
+    'give_credit': give_credit, # Give credit when a new model signs up
 }
 
+# Lambda handler function 
 def lambda_handler(event, context):
     try:
         operation = event['operation']
--- views.py
+++ views.py
@@ -57,24 +57,37 @@ def index(request):
 
 @require_login
 def logout(request):
-    del request.session['username']
-    return PAGES['logout']['method'](request)
+    try:
+        del request.session['username']
+        return PAGES['logout']['method'](request)
+    except Exception as e:
+        logger.error(f"Error in logout: {str(e)}", exc_info=True)
+        return JsonResponse({"error": "An error occurred during logout."}, status=500)
 
 @login_required
 def form_submit(request):
-    # Check for circular imports related issues 
-    # process form
-    return load_template('result.html', request)
+    try:
+        # Check for circular imports related issues 
+        # process form
+        return load_template('result.html', request)
+    except Exception as e:
+        logger.error(f"Error in form submit: {str(e)}", exc_info=True)
+        return JsonResponse({"error": "An error occurred during form submission."}, status=500)
 
 def login(request):
     if is_authenticated(request):
         return PAGES['index']['method'](request)
     if request.method == 'POST':
-        # Update login form processing methodology
-        return load_template('login.html', request)
+        try:
+            # Update login form processing methodology
+            return load_template('login.html', request)
+        except Exception as e:
+            logger.error(f"Error in login: {str(e)}", exc_info=True)
+            return JsonResponse({"error": "An error occurred while logging in."}, status=500)
 
 @login_required
 def social_media_login(request: HttpRequest, platform: str) -> Optional[HttpResponse]:
+    """This endpoint retrieves the social media login credentials for the specified platform."""
     try:
         api_keys = APICredentials.objects.filter(platform=platform)
         login_details = None
@@ -89,20 +102,34 @@ def social_media_login(request: HttpRequest, platform: str) -> Optional[HttpResp
 
 @login_required
 def oidc_auth(request: HttpRequest) -> Optional[HttpResponse]:
-    # OIDC logic
+    """This endpoint provides openid connect authentication."""
+    try:
+        # OIDC logic
+    except Exception as e:
+        logger.error(f"Error in oidc_auth: {str(e)}", exc_info=True)
+        return JsonResponse({"error": "An error occurred during openid connect authentication."}, status=500)
 
 @login_required
 def oidc_callback(request: HttpRequest) -> Optional[HttpResponse]:
-    # OIDC callback logic
+    """This endpoint process the openid connect callback."""
+    try:
+        # OIDC callback logic
+    except Exception as e:
+        logger.error(f"Error in oidc_callback: {str(e)}", exc_info=True)
+        return JsonResponse({"error": "An error occurred during openid connect callback."}, status=500)
 
 def render_page(request, page):
-    if page in PAGES:
-        page_cfg = PAGES[page]
-        if page_cfg.get('login_required', False) and not is_authenticated(request):
-            return redirect('login')
-        return page_cfg['method'](request)
-    else:
-        return Http404
+    try:
+        if page in PAGES:
+            page_cfg = PAGES[page]
+            if page_cfg.get('login_required', False) and not is_authenticated(request):
+                return redirect('login')
+            return page_cfg['method'](request)
+        else:
+            return Http404
+    except Exception as e:
+        logger.error(f"Error in render_page: {str(e)}", exc_info=True)
+        return JsonResponse({"error": "An error occurred while rendering the page."}, status=500)
 
 @login_required
 def serve(request, page):
@@ -110,14 +137,22 @@ def serve(request, page):
 
 @login_required
 def new_feature(request):
-    if 'admin' not in request.session['user'].groups: 
-        return load_template('not_authorised.html', request)
-
-    return PAGES['new_feature']['method'](request)
+    """Endpoint for admin-only new feature."""
+    try:
+        if 'admin' not in request.session['user'].groups: 
+            return load_template('not_authorised.html', request)
+        return PAGES['new_feature']['method'](request)
+    except Exception as e:
+        logger.error(f"Error in new_feature: {str(e)}", exc_info=True)
+        return JsonResponse({"error": "An error occurred while accessing the new feature."}, status=500)
 
 @login_required
 def another_new_feature(request):
-    if 'admin' not in request.session['user'].groups: 
-        return load_template('not_authorised.html', request)
-    
-    return PAGES['another_new_feature']['method'](request)
+    """Endpoint for admin-only another new feature."""
+    try:
+        if 'admin' not in request.session['user'].groups: 
+            return load_template('not_authorised.html', request)
+        return PAGES['another_new_feature']['method'](request)
+    except Exception as e:
+        logger.error(f"Error in another_new_feature: {str(e)}", exc_info=True)
+        return JsonResponse({"error": "An error occurred while accessing another new feature."}, status=500)
