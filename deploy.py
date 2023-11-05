3cec69f5c8413dad7825d4bedb3385ba1abfffc0
MutableAI <mutableai@github.com>
2023-11-05 09:33:56 +0000

    [code modification] containerize the whole spp

--- deploy.py
+++ deploy.py
@@ -1,11 +1,14 @@
 ```python
 import os
-import boto3
 import subprocess
 from botocore.exceptions import NoCredentialsError
 
 # AWS region
 AWS_REGION = os.environ.get('AWS_REGION')
+
+def install_docker():
+    """Install Docker on the local machine."""
+    subprocess.run(['curl', '-fsSL', 'https://get.docker.com', '|', 'sh'])
 
 # Function for installing Elastic Beanstalk CLI
 def install_ebcli():
@@ -15,24 +18,35 @@ def install_ebcli():
 # Function for initializing an Elastic Beanstalk environment
 def init_eb_environment():
     """Initializes an Elastic Beanstalk environment in the current directory."""
-    # Note: This needs additional user input that can't be automated, such as region selection
     subprocess.run(['eb', 'init'])
 
 # Function for creating Dockerrun.aws.json file
 def create_dockerrun():
     """Creates a Dockerrun.aws.json file for Elastic Beanstalk to understand 
-    how to deploy Docker container."""
+    how to deploy Docker container.
+
+    This configuration file is based on AWS Elastic Beanstalk multi-container Docker platform,
+    and it describes how to deploy a Docker container from a Dockerfile in your source bundle."""
     
     dockerrun_content = """
     {
         "AWSEBDockerrunVersion": "1",
+        "Image": {
+            "Name": "your Docker Hub account/image-name",
+            "Update": "true"
+        },
         "Ports": [
             {
-                "ContainerPort": "8000"
+                "ContainerPort": "8000",
+                "HostPort": "80"
             }
         ],
         "Volumes": [],
-        "Logging": "/var/log/nginx"
+        "Logging": "/var/log/nginx",
+        "environment": {
+            "AWS_ACCESS_KEY": "your AWS access key",
+            "AWS_SECRET_KEY": "your AWS secret key"
+        }
     }
     """
     
@@ -42,14 +56,16 @@ def create_dockerrun():
 # Function for deploying application to Elastic Beanstalk
 def eb_deploy():
     """Deploys the application to the Elastic Beanstalk environment."""
-    subprocess.run(['eb', 'create'])
+    subprocess.run(['eb', 'deploy'])
 
-def deploy():
+def main():
+    install_docker()
     install_ebcli()
     init_eb_environment()
     create_dockerrun()
     eb_deploy()
-    
+
 if __name__ == '__main__':
-    deploy()
+    main()
 ```
+<!-- This is the modified deploy.py script, filling the function of containerizing the app using Docker, installing necessary dependencies like the Elastic Beanstalk CLI, creating Dockerrun.aws.json with the necessary configuration, and deploying the app to AWS Elastic Beanstalk. Docker is installed via a simple curl command, and the Elastic Beanstalk CLI is installed via pip as per AWS's instructions. An environment is initialized via 'eb init', which will prompt the user to configure the environment. A Dockerrun.aws.json file is created, and its content is written directly within the script. Finally, the application is deployed using the 'eb deploy' command. -->
