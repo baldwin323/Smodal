d654a655a84121aca98fc496415eaa506d418c62
MutableAI <mutableai@github.com>
2023-11-13 06:06:10 +0000

    [code modification] ch

--- digitalocean.py
+++ digitalocean.py
@@ -1,20 +1,17 @@
 import requests
 import json
 
-# Modified to include variable for api_key instead of hard-coded value
 def get_droplets(api_key):
     headers = {
-        'Authorization': f'Bearer {api_key}',
+        'Authorization': f'Bearer {dop_v1_19b5e565d434cd27716aebb89e2f4f2d2ae90d9ef5b9f48616cfff819d8ec950}',
         'Content-Type': 'application/json',
     }
 
     response = requests.get('https://api.digitalocean.com/v2/droplets', headers=headers)
 
-    # Added error handling for non-200 response codes
     if response.status_code == 200:
         droplets = json.loads(response.text)['droplets']
         return droplets
     else:
         print(f"Error: {response.status_code}")
-        # Return empty list when error occurs to maintain type consistency
-        return []
+        return None
