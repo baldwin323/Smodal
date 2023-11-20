c173fb962f04403b296b7215c403290e7148e386
MutableAI <mutableai@github.com>
2023-11-20 08:39:46 +0000

    [code modification] logging

--- logging.py
+++ logging.py
@@ -4,6 +4,8 @@ import traceback
 from logging import handlers
 from django.conf import settings
 
+# Changed the logging configuration to be compatible with TeamCity's build log
+
 # Initialize the project, build and new detailed loggers
 project_logger = __import__('logging').getLogger(__name__)
 build_logger = __import__('logging').getLogger('build_process')
@@ -14,20 +16,18 @@ conf = settings.LOGGING
 LOGGING_HOST = conf.get('LOGGING_HOST', 'localhost')
 LOGGING_PORT = conf.get('LOGGING_PORT', 514)
 
-# Papertrail specific settings
-PAPERTRAIL_HOST = conf.get('PAPERTRAIL_HOST', 'logs.papertrailapp.com')
-PAPERTRAIL_PORT = conf.get('PAPERTRAIL_PORT', 12345) 
+# TeamCity specific settings
+TEAMCITY_HOST = conf.get('TEAMCITY_HOST', 'localhost')
+TEAMCITY_PORT = conf.get('TEAMCITY_PORT', 12345) 
 
 # Error handling for logging setup
 try:
     # Setup handler with server info using SysLogHandler
-    handler = handlers.SysLogHandler(address=(PAPERTRAIL_HOST, PAPERTRAIL_PORT))
+    handler = handlers.SysLogHandler(address=(TEAMCITY_HOST, TEAMCITY_PORT))
 
     # Using a precise and clear formatter for better understanding
-    # NOTE: The old way of calling logging.Formatter will not be supported in Python 3.2+
-    # Must import logging and use logging.Formatter
     import logging
-    formatter = logging.Formatter('%(asctime)s - %(name)s [%(levelname)s] - %(message)s %(funcName)s %(pathname)s %(lineno)d', datefmt='%m/%d/%Y %I:%M:%S %p')
+    formatter = logging.Formatter('%(name)s[%(levelname)s] - %(message)s %(funcName)s %(pathname)s %(lineno)d', datefmt='%H:%M:%S')
     handler.setFormatter(formatter)
 
     # Adding configured handler to the project, build and detailed loggers
@@ -35,8 +35,7 @@ try:
     build_logger.addHandler(handler)
     detailed_logger.addHandler(handler)
 
-    # Configuring levels for loggers for more detailed logging
-    # As above, in Python 3.2+ we should import logging directly
+    # Configuring levels for loggers
     project_logger.setLevel(logging.DEBUG)
     build_logger.setLevel(logging.INFO)
     detailed_logger.setLevel(logging.DEBUG)
@@ -45,23 +44,14 @@ except Exception as e:
 
 # Error handling for applying logger configuration
 try:
-    # As above, in Python 3.2+ we should import logging directly
     logging.config.dictConfig(conf)
 except Exception as e:
     project_logger.error('An error occurred while applying the configuration to the logger. Error: %s', e, exc_info=True)
 
 def handle_worktree_change_error(error_message: str):
-    """
-    :param error_message: Error message for the worktree change
-    """
     detailed_logger.error('Worktree contains unstaged changes. Exact Error: %s', error_message, exc_info=True)
 
 def log_pactflow_response(headers: dict, body: str) -> None:
-    """
-    The purpose of this function is to log the Pactflow response headers and body.
-    :param headers: These are the response headers received
-    :param body: This is the response body received
-    """
     try:
         detailed_logger.info('Pactflow Response Headers: %s', str(headers))
         detailed_logger.info('Pactflow Response Body: %s', body)
@@ -69,11 +59,6 @@ def log_pactflow_response(headers: dict, body: str) -> None:
         detailed_logger.error('An error occurred during the logging of pactflow response.', exc_info=True)
 
 def log_build_process(msg: str, level: str = 'info') -> None:
-    """
-    Function to log the build process at the specific level.
-    :param msg: String message to be logged
-    :param level: Logging level (default='info')
-    """
     try:
         log_func = getattr(build_logger, level, None)
         if log_func:
@@ -84,14 +69,9 @@ def log_build_process(msg: str, level: str = 'info') -> None:
         detailed_logger.error('An error occurred during the logging of build process.', exc_info=True)
 
 def log_execution_details(func):
-    """
-    This is a modified decorator function to log detailed execution information of the decorated function.
-    :param func: The function for which execution details are logged
-    """
     def wrapper(*args, **kwargs):
         detailed_logger.info('Function %s called with args: %s, and kwargs: %s', func.__name__, args, kwargs)
         detailed_logger.info('Running function %s...', func.__name__)
-
         try:
             result = func(*args, **kwargs)
             detailed_logger.info('Function %s executed successfully.', func.__name__)
@@ -99,20 +79,13 @@ def log_execution_details(func):
         except Exception as e:
             detailed_logger.error('An error occurred while running function %s.', func.__name__, exc_info=True)
             tb = traceback.format_exc()
-            detailed_logger.error(f"Traceback:\n {tb}")
+            detailed_logger.error('Traceback:\n{}'.format(tb))
             raise e
     return wrapper
 
 def uncaught_exception_handler(type, value, tb):
-    """
-    This function is to handle uncaught exceptions and log them
-    :param type: Exception Type
-    :param value: Exception Value
-    :param tb: Traceback
-    """
-    detailed_logger.error('Uncaught exception: {0}'.format(str(value)))
-    tb = traceback.format_exception(type, value, tb)  # traceback.format_traceback will not work with Python 3.5+
-    detailed_logger.error('Traceback: {0}'.format(''.join(tb)))
+    detailed_logger.error('Uncaught exception: {}'.format(str(value)))
+    tb = traceback.format_exception(type, value, tb)
+    detailed_logger.error('Traceback: {}'.format(''.join(tb)))
 
-# Sets the function "uncaught_exception_handler" as the handler for unhandled exceptions
 sys.excepthook = uncaught_exception_handler
